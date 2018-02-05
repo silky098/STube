7c02fbcf3942c237a04a0436e61f488923eb7273
silky098 <shaila@ccwd.com.au>
2018-02-05 13:22:34 +1100

    completed STube

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,5 @@
 import React, { Component } from 'react'
 
-const inputStyle = {
-  marginLeft: 5,
-  borderColor: '#DDDDDD',
-  borderWidth: 3,
-  borderRadius: 5,
-  backgroundColor: '#EEEEEE',
-  color: '#555555'
-}
-
 export default class SearchBar extends Component {
   constructor(props){
     super(props)
@@ -17,16 +8,20 @@ export default class SearchBar extends Component {
 
   render() {
     return (
-      <div style={{ color: 'gray', marginTop: 20 }}>
-        search
+      <div className='search-bar'>
+        <span className='search-text'>search
          <input
-          style={inputStyle}
+          className='input-style'
           value={this.state.term}
-          // controlled form element / component where value is set by state
-          onChange = {event => this.setState({ term: event.target.value })}
-          // event handler causes component to rerender and change value to new value
+          onChange = {event => this.onInputChange(event.target.value)}
         />
+      </span>
       </div>
     )
+  }
+
+  onInputChange(term) {
+    this.setState({term})
+    this.props.onSearchTermChange(term)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import _ from 'lodash'
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar'
+import VideoDetail from './components/video_detail'
 import VideoList from './components/video_list'
 const API_KEY = 'AIzaSyD29NoxVJkqQAKa1SVxUeTI5n-A5Ccu5Aw'
 
@@ -9,19 +11,39 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { videos: [] }
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos })
-      console.log(videos)
-    })
+    this.videoSearch('surfboards')
   }
+
+videoSearch(term) {
+  YTSearch({key: API_KEY, term: term}, (videos) => {
+    this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+    })
+  })
+}
+
   render() {
+
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term)}, 500)
+
     return (
       <div>
         <br />
-        <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <p className='h1 heading'>S.Tube</p>
+        <br />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos}
+        />
       </div>
     )
   }
